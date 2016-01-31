045de42945dbcf167e20c2f6c14a638ef738fa03
Jeremy Pollock <jpollock@tibco.com>
2016-01-31 09:57:41 -0800

    updating to include import external api definitions

--- masheryapi/scripts/import_external_api_definitions.py
+++ masheryapi/scripts/import_external_api_definitions.py
@@ -29,30 +29,49 @@ class ImportExternalApiDefinitions():
 
         imported_definition = {}
 
-        api_create_response = self.mashery_v3.create(access_token, '/services', 'fields=id,name,endpoints', api_definition)
+        api_response = self.mashery_v3.create(access_token, '/services', 'fields=id,name,endpoints', api_definition)
 
-        if api_create_response.status_code != 200:
-            raise ValueError("Some api problem.") 
+        if api_response.status_code != 200:
+            imported_definition['error'] = {}
+            imported_definition['error']['api'] = {}
+            imported_definition['error']['api']['code'] = api_response.status_code
+            imported_definition['error']['api']['message'] = api_response.json()
 
-        created_api = api_create_response.json()
+        else:
+            created_api = api_response.json()
 
-        imported_definition['api'] = {'id': created_api['id'], 'name': created_api['name']}
+            imported_definition['api'] = {'id': created_api['id'], 'name': created_api['name']}
 
-        if api_create_response.status_code == 200 and create_package == 'true':
-            package = {'name': created_api['name'], 'plans': [{'name': created_api['name'], 'services': [created_api]}]}
-            api_response = self.mashery_v3.create(access_token, '/packages', 'fields=id,name', package)
-            created_package = api_response.json()
-            imported_definition['package'] = {'id': created_package['id'], 'name': created_package['name']}
+            if api_response.status_code == 200 and create_package == 'true':
+                package = {'name': created_api['name'], 'plans': [{'name': created_api['name'], 'services': [created_api]}]}
+                api_response = self.mashery_v3.create(access_token, '/packages', 'fields=id,name', package)
+                if api_response.status_code != 200:
+                    if 'error'  not in imported_definition:
+                        imported_definition['error'] = {}
+                    imported_definition['error']['package'] = {}
+                    imported_definition['error']['package']['code'] = api_response.status_code
+                    imported_definition['error']['package']['message'] = api_response.json()
 
-        if api_create_response.status_code == 200 and create_iodoc == 'true':
-            iodoc_definition = api_service.iodoc_from_swagger(public_domain, external_api_definition_content)
+                else:
+                    created_package = api_response.json()
+                    imported_definition['package'] = {'id': created_package['id'], 'name': created_package['name']}
 
-            iodoc = {'defaultApi': False, 'serviceId': created_api['id'], 'definition': iodoc_definition}
+            if api_create_response.status_code == 200 and create_iodoc == 'true':
+                iodoc_definition = api_service.iodoc_from_swagger(public_domain, external_api_definition_content)
 
-            api_response = self.mashery_v3.create(access_token, '/iodocs/services', '', iodoc)
-            created_iodoc = api_response.json()
+                iodoc = {'defaultApi': False, 'serviceId': created_api['id'], 'definition': iodoc_definition}
 
-            imported_definition['iodoc'] = {'id': created_iodoc['serviceId'], 'defaultApi': created_iodoc['defaultApi']}
+                api_response = self.mashery_v3.create(access_token, '/iodocs/services', '', iodoc)
+                if api_response.status_code != 200:
+                    if 'error'  not in imported_definition:
+                        imported_definition['error'] = {}
+                    imported_definition['error']['iodocs'] = {}
+                    imported_definition['error']['iodocs']['code'] = api_response.status_code
+                    imported_definition['error']['iodocs']['message'] = api_response.json()
+                else:
+
+                    created_iodoc = api_response.json()
+                    imported_definition['iodoc'] = {'id': created_iodoc['serviceId'], 'defaultApi': created_iodoc['defaultApi']}
 
         return imported_definition
 
